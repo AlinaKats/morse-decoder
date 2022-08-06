42355da6f792538a3318e2aeca12d662f481b76f
Alina Kats <alina.emelianova91@gmail.com>
2022-08-06 17:11:48 +0300

    fix solution

--- src/index.js
+++ src/index.js
@@ -39,26 +39,21 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     let arrFromString = expr.match(/.{10}/g);
-    //console.log(arrFromString);
 
     //delete extra *00* from string;
     const deletedZero = arrFromString.map(char => char.replace(/00/g, ''));
-    console.log(deletedZero);
 
     //replace 10 and 11 to morse code string;
     let morseCode = [];
     for (let key of deletedZero) {
         let symbol = key.replace(/10/g, '.').replace(/11/g, '-');
         morseCode.push(symbol);
-        
     }
-    console.log(morseCode);
     
     /*= deletedZero.join('').replace(/10/g, '.').replace(/11/g, '-').split(',');
     console.log(morseCode);*/
 
-
-    //decode
+    //decode of array
     let result = [];
     for (let i = 0; i < morseCode.length; i++) {
         console.log(morseCode[i]);
@@ -71,23 +66,11 @@ function decode(expr) {
             result.push(' ');
         }
     }
+
     return result.join('');
 }
-decode('00000011110000000010');
 
 module.exports = {
     decode
 }
 
-/*
-
-**Строка ввода. Его длина кратна 10.
-- Каждая буква алфавита закодирована точками (`.`) и тире (`-`). 
-  `10` обозначает точку (`.`), `11` обозначает тире (`-`).
-- Каждая закодированная **длина буквы равна 10**.
-- Если длина закодированной буквы **меньше 10**, она "дополняется" слева "0".
-- `Пробел` в строке равен `************`.
-**Вывод:** Строка (декодированная)
-**Пример:** `me` -> `m === -- === 0000001111`, `e === . === 0000000010` -> `00000011110000000010`
-
-*/
